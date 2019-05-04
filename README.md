978a0eab86512148a54fd9eb6a38a0a6df7a0dff
jiwan ghosal <jiwan.cse@gmail.com>
2019-05-04 10:38:17 +0530

    adding get definations, getRealtedwords, getExample

--- README.md
+++ README.md
@@ -1,4 +1,52 @@
-# dic
+# Dic
 command line dictionary 
 
-npm link
+# Prerequisit
+> node --v
+
+        ```
+                v8.11.4
+        ```
+# Installation
+> Clone the repo
+
+        ```
+        git clone https://github.com/heartstchr/dic.git 
+        ```
+
+> Making index.js file executable 
+
+        ```
+        npm link
+        ```
+
+> Install the dependency
+
+        ```
+        npm i
+        ```
+
+> Command to get help
+
+```sh
+        dic --help
+
+        |  _ \  (_)   ___  | |_  (_)   ___    _ __     __ _   _ __   _   _ 
+        | | | | | |  / __| | __| | |  / _ \  | '_ \   / _` | | '__| | | | |
+        | |_| | | | | (__  | |_  | | | (_) | | | | | | (_| | | |    | |_| |
+        |____/  |_|  \___|  \__| |_|  \___/  |_| |_|  \__,_| |_|     \__, |
+                                                                |___/ 
+        Usage: dic [options] [command]
+
+        command line dictionary
+
+        Options:
+        -V, --version              output the version number
+        -h, --help                 output usage information
+
+        Commands:
+        getDefinations|def <word>  Get definations
+        getSynonyms|syn <word>     Get synonyms
+        getAntonyms|ant <word>     Get antonyms
+        getExamples|ex <word>      Get Examples
+```
--- index.js
+++ index.js
@@ -1,15 +1,49 @@
 #! /usr/bin/env node
-const chalk       = require('chalk');
-const clear       = require('clear');
-const figlet      = require('figlet');
+const chalk= require('chalk');
+const clear= require('clear');
+const figlet= require('figlet');
+const program= require('commander');
+
+const { getDefinations, getSynonyms, getAntonyms , getExamples} = require('./lib/index');
+
 clear();
 
 const run = async () => {
-    console.log(
-        chalk.yellow(
-          figlet.textSync('Dictionary', { horizontalLayout: 'full' })
-        )
-      );
-}
+	console.log(
+		chalk.yellow(
+			figlet.textSync('Dictionary', { horizontalLayout: 'full' })
+		)
+	);
 
-run()
+	program
+		.version('0.0.1')
+		.description('command line dictionary');
+
+	program
+		.command('getDefinations <word>')
+		.alias('def')
+		.description('Get definations')
+		.action(word => getDefinations(word));
+
+	program
+		.command('getSynonyms <word>')
+		.alias('syn')
+		.description('Get synonyms')
+		.action(word => getSynonyms(word));
+
+	program
+		.command('getAntonyms <word>')
+		.alias('ant')
+		.description('Get antonyms')
+		.action(word => getAntonyms(word));
+
+	program
+		.command('getExamples <word>')
+		.alias('ex')
+		.description('Get Examples')
+		.action(word => getExamples(word));
+
+	program.parse(process.argv);
+};
+
+run();
